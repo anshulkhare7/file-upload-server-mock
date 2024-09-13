3f03be4a75efb53821dff02bd9d7d3d3ea341df6
Anshul <anshulkhare7@gmail.com>
2024-09-13 20:05:05 +0530

    util for generating jwt token

---
+++ generateToken.js
@@ -0,0 +1,14 @@
+const jwt = require("jsonwebtoken");
+
+// Secret key to sign the JWT token (should match the one used in app.js)
+const JWT_SECRET = "honeycomb"; // Same as in your app.js
+
+// Payload (you can add user information here if needed)
+const payload = {
+  username: "anshul.khare",
+};
+
+// Generate the token
+const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "1h" });
+
+console.log("Generated Token:", token);
